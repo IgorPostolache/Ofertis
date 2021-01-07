8f657e1de5be217dd50ee5db2a11475712b6d717
IgorPostolache <igorpostolache90@gmail.com>
2021-01-07 14:08:38 +0100

    removed node_modules folder since it was not supposed to be uploaded in the first place but noticed late that I had badly configured the .gitignore.

--- angular-frontend/node_modules/@ngrx/entity/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { createEntityAdapter } from './create_adapter';
-export { Dictionary, EntityState, EntityAdapter, Update, EntityMap, EntityMapOne, Predicate, IdSelector, Comparer, } from './models';
